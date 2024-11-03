6c62c6c68773028e986eb336b8f45e47f75847c3
kzankpe <78028172+kzankpe@users.noreply.github.com>
2024-11-03 10:30:59 +0000

    feat(#20): add initial server code

--- unit-converter/main.go
+++ unit-converter/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
+
+	fmt.Println("Server is listening on port 8090")
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
