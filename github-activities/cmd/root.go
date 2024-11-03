35b13c3dddf801838ab7d01a4f67f3ca85840026
kzankpe <78028172+kzankpe@users.noreply.github.com>
2024-11-03 10:04:44 +0100

    fix(#2): update help description (#25)

--- github-activities/cmd/root.go
+++ github-activities/cmd/root.go
@@ -14,13 +14,12 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "github-activities",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Retrieves and displays the recent activity of a GitHub user in the terminal",
+	Long: `Retrieves and displays the recent activity of a GitHub user in the terminal.
+	 For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	gh-activities <username>
+	`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Args: cobra.ExactArgs(1),
