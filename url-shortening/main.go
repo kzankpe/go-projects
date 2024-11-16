eb1b713b03c1d0a21e87e6f868d10dac773b4001
kzankpe <78028172+kzankpe@users.noreply.github.com>
2024-11-16 20:54:29 +0100

    feat(#51): add postgres db configuration (#64)

--- url-shortening/main.go
+++ url-shortening/main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/kzankpe/go-projects/url-shortening/config"
+	"github.com/kzankpe/go-projects/url-shortening/models"
 	"github.com/kzankpe/go-projects/url-shortening/routes"
 )
 
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+	// Database configuration from env
+	config := config.Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBUserName: os.Getenv("DB_USER"),
+		DBUserPass: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBSslMode:  os.Getenv("DB_SSLMODE"),
+	}
+
 	// Initialize a router
 	r := routes.InitRouter()
 	routes.SetRoute(r)
+	// Connect to the Database
+	db, err := models.ConnectDB(config)
+	if err != nil {
+		log.Fatalf("Failed to connect to the databse %w", err)
+	}
+	err = db.AutoMigrate(&models.UrlData{})
+	if err != nil {
+		log.Fatalf("Error migrating the database: %v", err)
+	}
+	fmt.Println("Successfully connected to the database!")
 
 	// Run the server on a port
-	err := r.Run(":8090")
+	err = r.Run(":8090")
 	if err != nil {
 		panic(err)
 	}
--- url-shortening/models/models.go
+++ url-shortening/models/models.go
@@ -1,9 +1,16 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/kzankpe/go-projects/url-shortening/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 type UrlData struct {
-	ID         int       `json:"id"`
+	ID         uint      `json:"id" gorm:"primaryKey"`
 	Url        string    `json:"url"`
 	Shortcode  string    `json:"shortCode"`
 	CreatedAt  time.Time `json:"created_at"`
@@ -13,4 +20,14 @@ type UrlData struct {
 
 type OriginalUrl struct {
 	OUrl string `json:"url" validate:"required,notBlank"`
+}
+
+func ConnectDB(conf config.Config) (*gorm.DB, error) {
+	connectionStr := fmt.Sprintf("host=%s user=%s dbname=%s password=%s port=%s sslmode=%s", conf.DBHost, conf.DBUserName, conf.DBName, conf.DBUserPass, conf.DBPort, conf.DBSslMode)
+
+	db, err := gorm.Open(postgres.Open(connectionStr), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to the database %w", err)
+	}
+	return db, nil
 }
