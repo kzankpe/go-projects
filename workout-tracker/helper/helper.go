10edad5c253f05b0b6d398f6e3af5cb3294a3e55
kzankpe <78028172+kzankpe@users.noreply.github.com>
2024-12-22 16:41:35 +0100

    feat(#107): add function to verify password (#110)

--- workout-tracker/helper/helper.go
+++ workout-tracker/helper/helper.go
@@ -15,7 +15,6 @@ func HashPassword(password string) (string, error) {
 	return string(hashedPassword), nil
 }
 
-func VerifyPassword() {
-	// function to verify password
-
+func VerifyPassword(hashedPassword string, candidatePassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(candidatePassword))
 }
