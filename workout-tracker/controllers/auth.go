c0ceadf765f39b832095624712cb70cfaf35e779
kzankpe <78028172+kzankpe@users.noreply.github.com>
2024-12-22 17:14:03 +0100

    feat(#111): add signup logic to the project (#112)

--- workout-tracker/controllers/auth.go
+++ workout-tracker/controllers/auth.go
@@ -1,7 +1,13 @@
 package controllers
 
 import (
+	"net/http"
+	"strings"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/kzankpe/go-projects/workout-tracker/helper"
+	"github.com/kzankpe/go-projects/workout-tracker/models"
 	"gorm.io/gorm"
 )
 
@@ -16,7 +22,48 @@ func NewAuthController(DB *gorm.DB) AuthController {
 //SignUp user
 
 func (ac *AuthController) SignUpUser(c *gin.Context) {
-	//signup logic
+	var payload *models.SignUpInput
+	err := c.ShouldBindJSON(&payload)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+	if payload.Password != payload.PasswordConfirm {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": "Passwords do not match"})
+		return
+	}
+
+	hashedPassword, err := helper.HashPassword(payload.Password)
+	if err != nil {
+		c.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": err.Error()})
+		return
+	}
+
+	newUser := models.User{
+		Name:      payload.Name,
+		Email:     strings.ToLower(payload.Email),
+		Password:  hashedPassword,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	result := ac.DB.Create(&newUser)
+	if result.Error != nil && strings.Contains(result.Error.Error(), "duplicate key value violates unique") {
+		c.JSON(http.StatusConflict, gin.H{"status": "fail", "message": "User with that email already exists"})
+		return
+	} else if result.Error != nil {
+		c.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": "Something bad happened"})
+		return
+	}
+
+	userResponse := models.UserResponse{
+		ID:        newUser.ID,
+		Name:      newUser.Name,
+		Email:     newUser.Email,
+		CreatedAt: newUser.CreatedAt,
+		UpdatedAt: newUser.UpdatedAt,
+	}
+	c.JSON(http.StatusCreated, gin.H{"status": "success", "data": gin.H{"user": userResponse}})
 }
 
 //SignIn User
