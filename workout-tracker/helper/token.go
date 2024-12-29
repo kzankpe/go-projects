97ba6f7658d184657daad4551712bfe29e5581a9
kzankpe <78028172+kzankpe@users.noreply.github.com>
2024-12-29 12:14:42 +0100

    feat(#113): add generate token func (#116)

--- workout-tracker/helper/token.go
+++ workout-tracker/helper/token.go
@@ -1,7 +1,34 @@
 package helper
 
+import (
+	"encoding/base64"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
 // Generate Jwt
-func GenerateToken() (string, error) {
-	//function to generate jwt
-	return "", nil
+func GenerateToken(ttl time.Duration, payload interface{}, privateKey string) (string, error) {
+	decodedKey, err := base64.StdEncoding.DecodeString(privateKey)
+	if err != nil {
+		return "", fmt.Errorf("could not decode key: %w", err)
+	}
+
+	key, err := jwt.ParseRSAPrivateKeyFromPEM(decodedKey)
+	if err != nil {
+		return "", fmt.Errorf("create: parse key: %w", err)
+	}
+	now := time.Now().UTC()
+	tokenClaims := make(jwt.MapClaims)
+	tokenClaims["sub"] = payload
+	tokenClaims["exp"] = now.Add(ttl).Unix()
+	tokenClaims["iat"] = now.Unix()
+	tokenClaims["nbf"] = now.Unix()
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodRS256, tokenClaims).SignedString(key)
+	if err != nil {
+		return "", fmt.Errorf("create: sign token: %w", err)
+	}
+	return token, nil
 }
