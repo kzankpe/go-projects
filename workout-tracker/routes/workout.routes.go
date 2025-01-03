4302b164cb3a5fb685adfa9d165d02bddde0c4b7
kzankpe <78028172+kzankpe@users.noreply.github.com>
2025-01-03 09:58:17 +0000

    fix(#126): update workout group to use middleware

--- workout-tracker/routes/workout.routes.go
+++ workout-tracker/routes/workout.routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/kzankpe/go-projects/workout-tracker/controllers"
+	"github.com/kzankpe/go-projects/workout-tracker/middleware"
 )
 
 type WorkoutRouteController struct {
@@ -14,7 +15,7 @@ func NewWorkoutRouteController(workoutController controllers.WorkoutController) 
 }
 
 func (wrc *WorkoutRouteController) WorkoutRoute(rg *gin.RouterGroup) {
-	router := rg.Group("/workout")
+	router := rg.Group("/workout", middleware.AuthMiddleware())
 	router.POST("/create", wrc.WorkoutController.CreateWorkout)
 	router.PUT("/:workoutid", wrc.WorkoutController.UpdateWorkout)
 	router.DELETE("/:workoutid", wrc.WorkoutController.DeleteWorkout)
